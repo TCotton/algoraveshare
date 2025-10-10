c76bc9927d85f35d59e479f02f849a2f2c9ebba7
TCotton <me@andywalpole.me>
2025-10-10 17:51:43 +0100

    cleaned up unwanted code

--- algorave-design/src/components/AudioPlayer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AudioPlayer({ musicFile }) {
-    return (
-        <div className="audio-player">
-            <audio controls src={musicFile}/>
-        </div>
-    );
-}

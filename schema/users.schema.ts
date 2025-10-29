d36792e88307e047b02264ef7da56605793563b0
TCotton <me@andywalpole.me>
2025-10-29 11:41:28 +0000

    created users.schema.ts based on schema in SQL file

---
+++ schema/users.schema.ts
@@ -0,0 +1,15 @@
+import { Schema } from 'effect'
+
+export const UserSchema = Schema.Struct({
+  userId: Schema.UUID,
+  name: Schema.String,
+  email: Schema.String,
+  passwordHash: Schema.String,
+  location: Schema.NullOr(Schema.String),
+  createdAt: Schema.Date,
+  portfolioUrl: Schema.NullOr(Schema.String),
+  youtubeUrl: Schema.NullOr(Schema.String),
+  mastodonUrl: Schema.NullOr(Schema.String),
+  blueskyUrl: Schema.NullOr(Schema.String),
+  linkedinUrl: Schema.NullOr(Schema.String),
+})

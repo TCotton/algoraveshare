d2de02df85e853d9ba722f1a6801e21f30828455
TCotton <me@andywalpole.me>
2025-10-30 13:57:47 +0000

    made schema into standalone internal package

--- apps/backend/src/domain/projects/projects.api.ts
+++ apps/backend/src/domain/projects/projects.api.ts
@@ -1,3 +1,23 @@
 import { HttpApiGroup } from '@effect/platform'
 
 export class ProjectsApi extends HttpApiGroup.make('projects').prefix('/projects') {}
+
+/**
+ * import { HttpApiEndpoint } from "@effect/platform"
+ * import { Schema } from "effect"
+ * // Define a schema representing a User entity
+ * const User = Schema.Struct({
+ *   id: Schema.Number,
+ *   name: Schema.String,
+ *   createdAt: Schema.DateTimeUtc
+ * })
+ * // Define the "getUsers" endpoint, returning a list of users
+ * const getUsers = HttpApiEndpoint
+ *   //      ┌─── Endpoint name
+ *   //      │            ┌─── Endpoint path
+ *   //      ▼            ▼
+ *   .get("getUsers", "/users")
+ *   // Define the success schema for the response (optional).
+ *   // If no response schema is specified, the default response is `204 No Content`.
+ *   .addSuccess(Schema.Array(User))
+ */

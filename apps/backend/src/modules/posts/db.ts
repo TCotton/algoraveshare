94f0ba0ac1190f019f17c5749d7360bdc92cefb9
TCotton <me@andywalpole.me>
2025-11-15 19:18:24 +0000

    feat:added support for db

---
+++ apps/backend/src/modules/posts/db.ts
@@ -0,0 +1,15 @@
+// import * as DbSchema from '@repo/database/schema'
+/*
+import { desc } from 'drizzle-orm'
+import { db } from '../../db/index.js'
+
+export const getPosts = async () => {
+  const result = await db
+    .select()
+    .from(posts)
+    .orderBy(desc(posts.createdAt))
+    .limit(10)
+
+  return result
+}
+*/
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,62 @@
+import env from '@fastify/env'
+import dotenv from 'dotenv'
 import Fastify from 'fastify'
+import type { Level } from 'src/utils/logger.js'
+import { createLogger } from 'src/utils/logger.js'
 import { handler as lambdaHandler } from './index.js'
 
-const server = Fastify()
+dotenv.config()
+
+const level = process.env.PINO_LOG_LEVEL as Level
+const isDev = process.env.NODE_ENV === 'development'
+const logger = createLogger({ level, isDev })
+
+export { logger }
+
+const schema = {
+  type: 'object',
+  required: ['PORT', 'DATABASE_URL'],
+  properties: {
+    PORT: {
+      type: 'string',
+      default: 3000
+    },
+    DATABASE_URL: {
+      type: 'string'
+    },
+    PINO_LOG_LEVEL: {
+      type: 'string',
+      default: 'error'
+    },
+    NODE_ENV: {
+      type: 'string',
+      default: 'production'
+    }
+  }
+}
+
+const options = {
+  schema,
+  dotenv: true
+}
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    config: {
+      PORT: string
+      DATABASE_URL: string
+      PINO_LOG_LEVEL: string
+      NODE_ENV: string
+    }
+  }
+}
+
+const server = Fastify({
+  logger: true,
+  loggerInstance: logger
+})
+
+await server.register(env, options).after()
 
 server.get('/hello', async (request, reply) => {
   const res = await lambdaHandler(null)
